417faee866ffc90efa5a5220ab79ed3d14064423
Eric Romrell <eric_romrell@byu.edu>
2017-01-18 13:06:41 -0700

    Removed TODO

--- TransactionRegister/Model/Client/Amount.m
+++ TransactionRegister/Model/Client/Amount.m
@@ -18,8 +18,6 @@
 
 -(NSString *)formattedValue {
 	NSNumberFormatter *format = [NSNumberFormatter new];
-	//TODO: Figure out if you like this
-//	[format setNumberStyle:NSNumberFormatterCurrencyAccountingStyle];
 	[format setNumberStyle:NSNumberFormatterCurrencyStyle];
 	return [format stringFromNumber:[NSNumber numberWithDouble:self.value]];
 	return [NSString stringWithFormat:@"$%.02f", self.value];
